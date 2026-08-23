d33fe6a6a7e1cd22c2787f4c9da68ecdc4a4b998
reprateze <reprateze@gmail.com>
2026-08-22 23:02:21 -0300

    Incluindo markers nos testes

--- tests/UI/test_signup.py
+++ tests/UI/test_signup.py
@@ -4,11 +4,12 @@ pytestmark = pytest.mark.ui
 
 class TestSignup:
 
-
+    @pytest.mark.smoke
     def test_registro_com_sucesso(self, signup_page, email_unico, dados_cadastro):
         signup_page.registro(dados_cadastro["nome"], email_unico)
         assert signup_page.esta_na_segunda_etapa()
 
+    @pytest.mark.smoke
     def test_completar_formulario_cadastro(self, signup_page, dados_cadastro, email_unico):
         signup_page.registro(dados_cadastro["nome"], email_unico)
         assert signup_page.esta_na_segunda_etapa()
@@ -20,17 +21,19 @@ class TestSignup:
 
     # usar_email e um sinalizador para indicar ao teste se usar o email ou nao, foi a unica forma que achei para conseguir testar pois
     # como o parametrize rodam antes da fixture - ai precisei usar o email fixo quer foi gerado no teste da funcao abaixo
+    @pytest.mark.regression
     @pytest.mark.parametrize("nome, usar_email", [
         ("", True),           
         ("Renan Teste", False),  
         ("", False),           
         ])
-    
+        
     def test_registro_com_campos_vazios(self, signup_page, email_unico, nome, usar_email):
         email = email_unico if usar_email else ""
         signup_page.registro(nome, email)
         assert not signup_page.esta_na_segunda_etapa()
 
+    @pytest.mark.regression
     def test_email_repetido(self, signup_page, email_unico, dados_cadastro):
         signup_page.registro(dados_cadastro["nome"], email_unico)
         assert signup_page.esta_na_segunda_etapa()
